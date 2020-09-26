665933eaba0309f9f00caf1390e4ab5d0a9d6c4d
Mustafa Masetic <mustafa.masetic@sulzer.de>
2020-09-26 15:19:50 +0200

    Fix serving local page

--- wdio.conf.js
+++ wdio.conf.js
@@ -62,7 +62,7 @@ exports.config = {
         'goog:chromeOptions': {
             // to run chrome headless the following flags are required
             // (see https://developers.google.com/web/updates/2017/04/headless-chrome)
-           // args: ['--headless'],
+            args: ['--headless'],
             },
         acceptInsecureCerts: true
         // If outputDir is provided WebdriverIO can capture driver session logs
