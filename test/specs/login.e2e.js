27577edd62ff0fff2609f77a23989f691627199d
Mustafa Masetic <mustafa.masetic@sulzer.de>
2020-09-26 11:21:27 +0200

    Remove comment

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -6,6 +6,5 @@ describe('webdriver.io page', () => {
     it('should have the right title', async () => {
         await browser.url(`file:///${rootFolder}src/DE/LoginPage.html`)
         await percySnapshot(browser, 'My login page')
-        //expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js');
     })
 })
