c380ebf5c8eb5919956e9a942db99c9172f7dcf0
Mustafa Masetic <mustafa.masetic@sulzer.de>
2020-09-27 10:24:16 +0200

    Improved test with google maps

--- test/specs/map.e2e.js
+++ test/specs/map.e2e.js
@@ -3,6 +3,7 @@ const { percySnapshot } = require('@percy/webdriverio')
 describe('webdriver.io page', () => {
     it('should see google maps page', async () => {
         await browser.url('http://localhost:8080/Map.html')
+        await browser.pause(3000);
         await percySnapshot(browser, 'Google maps page', { widths: [480, 768, 992, 1200] })
     })
 })
