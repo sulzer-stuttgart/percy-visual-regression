806b325de2f0d2ed24a4d2d40e8f261aa7a55e6a
Mustafa Masetic <mustafa.masetic@sulzer.de>
2020-09-27 10:11:31 +0200

    Add Google Maps Test

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -2,10 +2,8 @@
 const { percySnapshot } = require('@percy/webdriverio')
 
 describe('webdriver.io page', () => {
-    it('should have the right title', async () => {
+    it('should see login page', async () => {
         await browser.url('http://localhost:8080/LoginPage.html')
-        browser.pause(3000)
         await percySnapshot(browser, 'My login page')
-        //expect(browser).toHaveTitle('WebdriverIO · Next-gen browser and mobile automation test framework for Node.js');
     })
 })
