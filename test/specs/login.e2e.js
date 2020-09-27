f6685bbbd2163314145b1c6e8365e1931c88a4a1
Mustafa Masetic <mustafa.masetic@sulzer.de>
2020-09-27 13:48:12 +0200

    Fix login tests

--- test/specs/login.e2e.js
+++ test/specs/login.e2e.js
@@ -4,28 +4,39 @@ const { percySnapshot } = require('@percy/webdriverio')
 describe('Login page', () => {
     it('should see login page', async () => {
         await browser.url('http://localhost:8080/LoginPage.html')
+        await browser.pause(1000)
         await percySnapshot(browser, 'My login page')
     });
 
     it('Failed login without password and email typing', async () => {
         await browser.url('http://localhost:8080/LoginPage.html')
-        await browser.elementClick('.button');
+        let button = await browser.$('.button')
+        await button.click()
+        await browser.pause(1000)
         await percySnapshot(browser, 'Failed login without password and email typing')
     });
     
     it('Failed login', async () => {
         await browser.url('http://localhost:8080/LoginPage.html')
-        await browser.setValue('#email', 'mustafa@sulzer.de');
-        await browser.setValue('#password', 'ksjdfhkjdfsdf');
-        await browser.elementClick('.button')
+        let email = await browser.$('#email')
+        let password = await browser.$('#password')
+        let button = await browser.$('.button')
+        await email.setValue('mustafa@sulzer.de')
+        await password.setValue('ksjdfhkjdfsdf')
+        await button.click()
+        await browser.pause(1000)
         await percySnapshot(browser, 'Failed login')
     });
     
     it('Successful login', async () => {
         await browser.url('http://localhost:8080/LoginPage.html')
-        $('#email').setValue('mustafa@sulzer.de')
-        $('#password').setValue('Password1!')
-        await browser.elementClick('.button');
+        let email = await browser.$('#email')
+        let password = await browser.$('#password')
+        let button = await browser.$('.button')
+        await email.setValue('mustafa@sulzer.de')
+        await password.setValue('Password1!')
+        await button.click()
+        await browser.pause(1000)
         await percySnapshot(browser, 'Successful login')
     });
     
